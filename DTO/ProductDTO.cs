a9c73a977b0956d8d4cd848a2d3131240c62ab13
Diego Fernando Nuñez Sanchez <dnunesan@emeal.nttdata.com>
2023-07-04 11:22:30 -0500

    Implementacion DTO e interface

--- DTO/ProductDTO.cs
+++ DTO/ProductDTO.cs
@@ -6,7 +6,7 @@ namespace mli_microNetCore_StoredProcedure.DTO
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }       
+        public int Price { get; set; }
         public string SKU { get; init; }
     }
 }
